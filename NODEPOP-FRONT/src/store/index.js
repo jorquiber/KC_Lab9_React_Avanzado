6dc8494b6990b513e5bed80ff66a022e4ace8c2b
Jorge Q. <jqb94@hotmail.com>
2024-06-23 16:16:23 +0200

    Configuring redux dev tools

--- NODEPOP-FRONT/src/store/index.js
+++ NODEPOP-FRONT/src/store/index.js
@@ -4,21 +4,21 @@ import * as reducers from './reducers';
 import * as auth from '../pages/auth/service';
 import * as adverts from '../pages/adverts/service';
 import { failureRedirects } from './middleware';
+import { composeWithDevTools, actionCreators } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers(reducers);
 
-console.log('auth', auth);
-console.log('adverts', adverts);
+const composeEnhancers = composeWithDevTools({ actionCreators });
 
 export default function configureStore(preloadedState = {}, { router }) {
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(withExtraArgument({ services: { auth, adverts }, router }), 
+    composeEnhancers(applyMiddleware(withExtraArgument({ services: { auth, adverts }, router }), 
       failureRedirects(router, {
       401: '/login',
       404: '/404',
-    }),));
+    }))));
 
   return store;
 }
