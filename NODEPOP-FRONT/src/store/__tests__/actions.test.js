00511f9d5ad7f8d5a3bf3ff7bf866fbf2f06ce8f
Jorge Q. <jqb94@hotmail.com>
2024-06-23 18:59:34 +0200

    Testing async action

--- NODEPOP-FRONT/src/store/__tests__/actions.test.js
+++ NODEPOP-FRONT/src/store/__tests__/actions.test.js
@@ -2,11 +2,59 @@ import * as actions from '../actions';
 import * as types from '../types';
 
 describe('authLoginFulfilled sync action', () => {
-  it('should return an "AUTH_LOGIN_FULFILLED" action', () => {
+  test('should return an "AUTH_LOGIN_FULFILLED" action', () => {
     const expectedAction = {
       type: types.AUTH_LOGIN_FULFILLED,
     };
     const action = actions.authLoginFulfilled();
     expect(action).toEqual(expectedAction);
+  });
+});
+
+
+describe('loadAdverts async action', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks(); 
+      });
+
+    const mockAdverts = [{ id: 1, title: 'Advert 1' }, { id: 2, title: 'Advert 2' }];
+    const dispatch = jest.fn();
+    const getState = jest.fn(() => ({
+        adverts: {
+          loaded: false,
+          data: [],
+        },
+      }));
+    const services = {
+        adverts: {
+          getAdverts: jest.fn().mockResolvedValue(mockAdverts),
+        },
+      };
+
+    const action = actions.loadAdverts();
+
+  test('should dispatch ADVERTS_LOADED_FULFILLED when adverts are loaded', async () => {
+    
+    await action(dispatch, getState, { services });
+    
+    expect(getState).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, actions.advertsLoadedPending());
+    expect(services.adverts.getAdverts).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenNthCalledWith(2, actions.advertsLoadedFulfilled(mockAdverts));
+  });
+
+  test('should dispatch ADVERTS_LOADED_REJECTED when there is an error getting adverts', async () => {
+    const error = new Error('API error');
+    services.adverts.getAdverts.mockRejectedValue(error);
+
+    await action(dispatch, getState, { services });
+    
+    expect(getState).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(1, actions.advertsLoadedPending());
+    expect(services.adverts.getAdverts).toHaveBeenCalled();
+    expect(dispatch).toHaveBeenNthCalledWith(2, actions.advertsLoadedRejected(error));
   });
 });
