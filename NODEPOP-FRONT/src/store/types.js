443ec3667cb5f5ea7d20ac578ecbeeeaa2c51f78
Jorge Q. <jqb94@hotmail.com>
2024-06-23 12:25:21 +0200

    Define states for redux

---
+++ NODEPOP-FRONT/src/store/types.js
@@ -0,0 +1,23 @@
+export const AUTH_LOGIN_PENDING = 'auth/login/pending';
+export const AUTH_LOGIN_FULFILLED = 'auth/login/fulfilled';
+export const AUTH_LOGIN_REJECTED = 'auth/login/rejected';
+
+export const AUTH_LOGOUT = 'auth/logout';
+
+export const ADVERTS_LOADED_PENDING = 'adverts/loaded/pending';
+export const ADVERTS_LOADED_FULFILLED = 'adverts/loaded/fulfilled';
+export const ADVERTS_LOADED_REJECTED = 'adverts/loaded/rejected';
+
+export const ADVERTS_DETAIL_PENDING = 'adverts/detail/pending';
+export const ADVERTS_DETAIL_FULFILLED = 'adverts/detail/fulfilled';
+export const ADVERTS_DETAIL_REJECTED = 'adverts/detail/rejected';
+
+export const ADVERTS_CREATED_PENDING = 'adverts/created/pending';
+export const ADVERTS_CREATED_FULFILLED = 'adverts/created/fulfilled';
+export const ADVERTS_CREATED_REJECTED = 'adverts/created/rejected';
+
+export const TAGS_LOADED_PENDING = 'tags/loaded/pending';
+export const TAGS_LOADED_FULFILLED = 'tags/loaded/fulfilled';
+export const TAGS_LOADED_REJECTED = 'tags/loaded/rejected';
+
+export const UI_RESET_ERROR = 'ui/reset-error';
